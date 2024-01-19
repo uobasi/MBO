8184ab363e6de42142da6619232c779b34374ef1
uobasi <54387747+uobasi@users.noreply.github.com>
2024-01-19 01:06:28 -0500

    .

--- app.py
+++ app.py
@@ -18,8 +18,11 @@ pio.renderers.default='browser'
 import timeit
 
     
-FutureMBOSymbolList = ['ESH4','NQH4', 'CLG4', 'GCG4', '6AH4','6BH4','6CH4', '6EH4', '6JH4', '6SH4', '6NH4']
-FutureMBOSymbolNumList = ['17077', '750','686071', '41512', '156755', '156618', '1545', '156627', '156657', '156650', '2259',]
+FutureMBOSymbolList = ['ESH4','NQH4', 'CLG4', 'GCG4',]
+FutureMBOSymbolNumList = ['17077', '750','686071', '41512',]
+
+currMBOSymbolList = ['6AH4','6BH4','6CH4', '6EH4', '6JH4', '6SH4', '6NH4']
+currMBOSymbolNumList =  ['156755', '156618', '1545', '156627', '156657', '156650', '2259',]
 
 #stkName = 'NQH4'  
 
@@ -70,19 +73,39 @@ def update_graph_live(n_intervals, data):
 
     if data in FutureMBOSymbolList:
         stkName = data
-        symbolNum = FutureMBOSymbolNumList[FutureMBOSymbolList.index(stkName)] 
+        symbolNum = FutureMBOSymbolNumList[FutureMBOSymbolList.index(stkName)]
+        
+    elif data in currMBOSymbolList:
+        stkName = data
+        symbolNum = currMBOSymbolNumList[currMBOSymbolList.index(stkName)]
+        
     else:
         stkName = 'NQH4'  
         symbolNum = FutureMBOSymbolNumList[FutureMBOSymbolList.index(stkName)]
         
-       
-    gclient = storage.Client(project="stockapp-401615")
-    bucket = gclient.get_bucket("stockapp-storage")
-    blob = Blob('levelTwoMBO', bucket) 
-    levelTwoMBO = blob.download_as_text()
     
+    if stkName in FutureMBOSymbolNumList:
+        gclient = storage.Client(project="stockapp-401615")
+        bucket = gclient.get_bucket("stockapp-storage")
+        blob = Blob('levelTwoMBO', bucket) 
+        levelTwoMBO = blob.download_as_text()
+        
+        
+        csv_reader  = csv.reader(io.StringIO(levelTwoMBO))
+        
+    elif stkName in currMBOSymbolList:
+        gclient = storage.Client(project="stockapp-401615")
+        bucket = gclient.get_bucket("stockapp-storage")
+        blob = Blob('levelTwoMBOCurr', bucket) 
+        levelTwoMBO = blob.download_as_text()
+        
+        
+        csv_reader  = csv.reader(io.StringIO(levelTwoMBO))
+        
+        
     
-    csv_reader  = csv.reader(io.StringIO(levelTwoMBO))
+        
+    
 
 
     csv_rows = []
