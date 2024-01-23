e9728e03692b14fc14455e4dc7948fdcbb418214
uobasi <54387747+uobasi@users.noreply.github.com>
2024-01-23 18:51:05 -0500

    .

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def update_graph_live(n_intervals, data):
     minAgg2 = []
     for i in levelTwoMBO:
         if i[4] == 'T':
-            if int(levelTwoMBO[0][0]) - (60000000000*5) <= int(i[0]):
+            if int(levelTwoMBO[0][0]) - (60000000000*10) <= int(i[0]):
                 minAgg2.append(i)
                 
     dic2 = {}
