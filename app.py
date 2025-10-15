bf63e6c4bfb4caa4cbefa1bb8fb958018ef30376
245 <54387747+uobasi@users.noreply.github.com>
2025-10-15 05:14:26 -0400

    .

--- app.py
+++ app.py
@@ -49,10 +49,20 @@ app.layout = html.Div([
         n_intervals=0,
       ),
 
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',children="Enter a symbol from |'ES' 'NQ'| and submit"),
-    dcc.Store(id='stkName-value')
+    #html.Div(dcc.Input(id='input-on-submit', type='text')),
+    #html.Button('Submit', id='submit-val', n_clicks=0),
+    #html.Div(id='container-button-basic',children="Enter a symbol from |'ES' 'NQ'| and submit"),
+    #dcc.Store(id='stkName-value')
+
+    html.Div([
+        html.Div([
+            dcc.Input(id='input-on-submit', type='text', className="input-field"),
+            html.Button('Submit', id='submit-val', n_clicks=0, className="submit-button"),
+            html.Div(id='container-button-basic', children="Enter a symbol from ES, NQ", className="label-text"),
+        ], className="sub-container"),
+        dcc.Store(id='stkName-value'),
+
+    ], className="main-container"),
 ])
 
 @callback(
